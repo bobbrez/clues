6ff82d6e1df23d1553d0bfd6e3599f4248a97224
Bob Breznak <143331+bobbrez@users.noreply.github.com>
2024-05-01 11:20:59 -0400

    feat: add more clues

--- src/app/clues.ts
+++ src/app/clues.ts
@@ -22,10 +22,22 @@ export const CLUES = [
     image: "04-suspect-tim.svg",
     board: "04",
   },
-  {
-    description:
-      'A letter is found in the victim\'s pocket. It reads: \n\n"It will be to your advantage to be present on this date because a Mr. Boddy will bring to an end a certain long-standing confidential and painful financial liability."\n\nIt is signed, "A friend."',
-    image: "05-letter.svg",
-    board: "05",
-  },
+  // {
+  //   description:
+  //     'A letter is found in the victim\'s pocket. It reads: \n\n"It will be to your advantage to be present on this date because a Mr. Boddy will bring to an end a certain long-standing confidential and painful financial liability."\n\nIt is signed, "A friend."',
+  //   image: "05-letter.svg",
+  //   board: "05",
+  // },
+  // {
+  //   description:
+  //     "Reviewing the footage from the W Boston Hotel, you see a gathering of people in the lobby who are interacting with the victim.\n\nYou first interview Mrs. Bianco who states she was at the hotel for a business meeting. She states she left the hotel at 6:30 PM and went to the AMC Downtown to watch a movie, but was unable to recall which movie she saw. She has no alibi for the time of the approximate time of death.\n\nIt is worth noting that her late husband, a renowned nuclear physicist, was found dead under mysterious circumstances. Her previous husband, an illusionist, went missing and has never been found.",
+  //   image: "06-bianco.svg",
+  //   board: "06",
+  // },
+  // {
+  //   description:
+  //     "You next interview Mr. Chartreuse. He states that he is in town for work but does not offer any further details. He is out of town from Washington DC and has no alibi for the time of the approximate time of death. You search his room and find a bloody shirt in his suitcase. He states that he cut himself shaving.",
+  //   image: "07-chartreuse.svg",
+  //   board: "07",
+  // },
 ];
